856c8215256d0775d334cb7ed8ba734050a46abf
sidetweet <@sidetweet>
2013-02-09 02:51:22 -0500

    sword url

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,7 +1,9 @@
 Todos.Todo = DS.Model.extend({
 	title: DS.attr('string'),
 	isCompleted: DS.attr('boolean'),
-
+	urlFromTitle: function() {
+		return "sword://KJV/" + this.get('title');
+	}.property('title'),
 	todoDidChange: function() {
 		Ember.run.once(this, function() {
 			this.get('store').commit();
