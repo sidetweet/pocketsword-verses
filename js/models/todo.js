9955973294151ae72edcdc1a073202fbf055f9cb
sidetweet <@sidetweet>
2013-02-09 19:59:22 -0500

    open last version instead of KJV

--- js/models/todo.js
+++ js/models/todo.js
@@ -2,7 +2,7 @@ Todos.Todo = DS.Model.extend({
 	title: DS.attr('string'),
 	isCompleted: DS.attr('boolean'),
 	urlFromTitle: function() {
-		return "sword://KJV/" + this.get('title');
+		return "sword:///" + this.get('title');
 	}.property('title'),
 	todoDidChange: function() {
 		Ember.run.once(this, function() {
